befe99ec49614ca45fc50a87f0fde9eb95fc9a84
Yasmin Abdelhalim <yasminabdelhalim44@gmail.com>
2025-05-08 23:27:44 -0700

    delete just for files

--- script.js
+++
@@ -1,7 +0,0 @@
-const buyButtons = document.querySelectorAll('.buy-now');
-
-buyButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        alert("Product added to your cart!"); 
-    });
-});
